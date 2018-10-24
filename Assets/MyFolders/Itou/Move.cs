d93aae4e2f38cf7f6a8b56b4e0c620faa91a576f
Kaito Itou <nkc.kaito.itou@gmail.com>
2018-10-24 14:07:09 +0900

    プレイヤー用のスクリプトを作成

---
+++ Assets/MyFolders/Itou/Move.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Move : MonoBehaviour
+{
+    [SerializeField]
+    float speed;
+    [SerializeField]
+    float jumpPower;
+    Rigidbody rigid;
+	// Use this for initialization
+	void Start ()
+    {
+        rigid = GetComponent<Rigidbody>();
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        //左右移動
+        if (Input.GetAxis("Horizontal") != 0)
+        {
+            transform.position += new Vector3(Input.GetAxis("Horizontal") * speed * Time.deltaTime, 0, 0);
+        }
+        //ジャンプ
+        if (Input.GetButtonDown("Jump"))
+        {
+            rigid.AddForce(transform.up * jumpPower);
+        }
+	}
+}
